afeb19403944811778a0d5c50041bdbfa36b9444
N0Tamine <amine450works@gmail.com>
2023-11-07 00:10:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./Header.jsx";
+import Infoperso from "./Infoperso.jsx";
+import Formation from "./Formation.jsx";
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <body>
+        <div className="all" >
+          <Header></Header>
+          <Infoperso></Infoperso>
+          <Formation></Formation>
+        </div>
+      </body>
   );
 }
 
-export default App;
+export default App
